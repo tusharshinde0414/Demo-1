837fcfbc5d989fd16203a6f333fe63f229cb3ed6
Tushar Shinde <tushar.shinde0414@gmail.com>
2024-12-13 19:51:12 +0530

    New files

--- First.js
+++ First.js
@@ -1,1 +1,20 @@
-console.log("Tushar Shinde");
+let result = document.getElementById("result");
+
+// Append value to the screen
+function appendValue(value) {
+  result.value += value;
+}
+
+// Clear the screen
+function clearScreen() {
+  result.value = "";
+}
+
+// Calculate the result
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    alert("Invalid Input!");
+  }
+}
---
+++ Python.py
@@ -0,0 +1,13 @@
+# a = 10
+# b = 14
+# print(a % b)
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b) 
+# print(a ** b)
+# input("Enter your name:")
+a = input("Enter value a :")
+b = input("Enter value b :")
+sum = a + b
+print(sum)
